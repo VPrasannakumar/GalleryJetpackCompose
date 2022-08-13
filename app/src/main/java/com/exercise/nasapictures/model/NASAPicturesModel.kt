63c00d5a220dbae7f211a1059ffc730efd011c5e
VPrasannakumar <prasannakumar.nalam@gaiansolutions.com>
2022-08-13 21:21:10 +0530

    Implemenation jetpack compose, create a data model class

---
+++ app/src/main/java/com/exercise/nasapictures/model/NASAPicturesModel.kt
@@ -0,0 +1,17 @@
+package com.exercise.nasapictures.model
+
+//Model class for NASA Pictures data
+data class NASAPicturesModel(
+
+    val copyright : String,
+
+    val date : String,
+
+    val explanation : String,
+
+    val hdUrl : String,
+
+    val title : String,
+
+
+)
---
+++ app/src/main/java/com/exercise/nasapictures/ui/components/MainContent.kt
@@ -0,0 +1,2 @@
+package com.exercise.nasapictures.ui.components
+

50efa7a548246c9d69cea62dedd679c00c7c8229
VPrasannakumar <prasannakumar.nalam@gaiansolutions.com>
2022-08-15 13:40:20 +0530

    Add a theme

--- app/src/main/java/com/exercise/nasapictures/ui/components/MainScreen.kt
+++ app/src/main/java/com/exercise/nasapictures/ui/components/MainScreen.kt
@@ -1,42 +1,37 @@
 package com.exercise.nasapictures.ui.components
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.content.res.Resources
 import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.exercise.nasapictures.R
 import com.exercise.nasapictures.model.NASAPicturesModel
 import com.exercise.nasapictures.ui.Screen
-import com.exercise.nasapictures.ui.theme.Purple500
 import com.exercise.nasapictures.util.FetchJSONFromAsset
+import com.plcoding.navigationdrawercompose.ui.theme.NASAPicturesComposeTheme
 
 @Composable
 fun MainContent(navController: NavController) {
-    //Adding grid view
 
-    gridView(LocalContext.current, navController)
+    NASAPicturesComposeTheme {
+        //Adding grid view
+        gridView(LocalContext.current, navController)
+    }
 
 }
 
@@ -44,7 +39,7 @@ fun MainContent(navController: NavController) {
 // on below line we are creating grid view function for loading our grid view.
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
-fun gridView(context: Context,navController: NavController) {
+fun gridView(context: Context, navController: NavController) {
     // on below line we are creating and initializing our array list
     lateinit var picturesList: List<NASAPicturesModel>
     picturesList = FetchJSONFromAsset.parseNASAPicturesJSON(context, "data.json")
@@ -85,7 +80,7 @@ fun gridView(context: Context,navController: NavController) {
                     // Adding vertical arrangement for our column
                     verticalArrangement = Arrangement.Center
                 ) {
-                 // on below line we are creating image for our grid view item.
+                    // on below line we are creating image for our grid view item.
                     AsyncImage(
                         model = ImageRequest.Builder(LocalContext.current)
                             .data(picturesList[it].hdUrl)
